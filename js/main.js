2658dfe44b0a27a4f22561d57cf51053656b3b70
selistym <selistym@protonmail.com>
2018-12-26 23:05:17 +0200

    zoom in/out, highlight area.

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
 // Define size of map group
 // Full world map is 2:1 ratio
 // Using 12:5 because we will crop top and bottom of map
+var color_normal = "#00496e";
+var color_selected = "#00886e";
 var dealers = [];// distributor info json array.
 var json_data = [];//total json datas
 w = 3000;
@@ -10,14 +12,18 @@ h = 1250;
 var minZoom;
 var maxZoom;
 
+//for showing tooltip
+var div = d3.select("body").append("div")
+  .attr("class", "tooltip")
+  .style("opacity", 0);
+
 // DEFINE FUNCTIONS/OBJECTS
 // Define map projection
 var projection = d3
   .geoEquirectangular()
   .center([0, 15]) // set centre to further North as we are cropping more off bottom of map
   .scale([w / (2 * Math.PI)]) // scale to fit group width
-  .translate([w / 2, h / 2]) // ensure centred in group
-  ;
+  .translate([w / 2, h / 2]); // ensure centred in group  
 
 // Define map path
 var path = d3
@@ -153,75 +159,28 @@ d3.json(
       .attr("class", "country")
       // add a mouseover action to show name label for feature/country
       .on("mouseover", function (d, i) {
-        d3.select("#countryLabel" + d.properties.iso_a3).style("display", "block");
-        // d3.selectAll(".country").classed("country-on", false);
-        // d3.select(this).classed("country-on", true);
-      })
-      .on("mouseout", function (d, i) {
-        d3.select("#countryLabel" + d.properties.iso_a3).style("display", "none");
-      })
-      // add an onclick action to zoom into clicked country
-      .on("click", function (d, i) {
-        show_info(d.properties.iso_n3)
-        d3.selectAll(".country").classed("country-on", false);
-        d3.select(this).classed("country-on", true);        
-        boxZoom(path.bounds(d), path.centroid(d), 20);
-      });
-    // Add a label group to each feature/country. This will contain the country name and a background rectangle
-    // Use CSS to have class "countryLabel" initially hidden
-    countryLabels = countriesGroup
-      .selectAll("g")
-      .data(json_data.features)
-      .enter()
-      .append("g")
-      .attr("class", "countryLabel")
-      .attr("id", function (d) {
-        return "countryLabel" + d.properties.iso_a3;
-      })
-      .attr("transform", function (d) {
-        return (
-          "translate(" + path.centroid(d)[0] + "," + path.centroid(d)[1] + ")"
-        );
-      })
-      // add mouseover functionality to the label
-      .on("mouseover", function (d, i) {
-        d3.select(this).style("display", "block");
+
+        d3.select(this).transition().style("opacity", 1);
         d3.select(this).style("cursor", "pointer");
+        div.transition()
+          .duration(200)
+          .style("opacity", .9);
+        div.html(d.properties.formal_en)
+          .style("left", (d3.event.pageX) + 5 / 1 + "px")
+          .style("top", (d3.event.pageY) + 5 / 1 + "px");
       })
       .on("mouseout", function (d, i) {
-        d3.select(this).style("display", "none");
+        d3.select(this).transition().style("opacity", 0.7);
+        div.transition()
+          .duration(2000)
+          .style("opacity", 0);
       })
       // add an onclick action to zoom into clicked country
       .on("click", function (d, i) {
         show_info(d.properties.iso_n3);
-        d3.selectAll(".country").classed("country-on", false);
-        d3.select("#country" + d.properties.iso_a3).classed("country-on", true);
-        boxZoom(path.bounds(d), path.centroid(d), 20);
-      });
-    // add the text to the label group showing country name
-    countryLabels
-      .append("text")
-      .attr("class", "countryName")
-      .style("text-anchor", "middle")
-      .attr("dx", 0)
-      .attr("dy", 0)
-      .text(function (d) {
-        //console.log(d.properties.name)
-        return d.properties.name;
-      })
-      .call(getTextBox);
-    // add a background rectangle the same size as the text
-    countryLabels
-      .insert("rect", "text")
-      .attr("class", "countryLabelBg")
-      .attr("transform", function (d) {
-        return "translate(" + (d.bbox.x - 2) + "," + d.bbox.y + ")";
-      })
-      .attr("width", function (d) {
-        return d.bbox.width + 4;
-      })
-      .attr("height", function (d) {
-        return d.bbox.height;
+        d3.selectAll(".country").style("fill", "#00496e");
+        d3.select(this).transition().style("fill", color_selected);
+        boxZoom(path.bounds(d), path.centroid(d), 100);
       });
     initiateZoom();
   }
@@ -257,15 +216,23 @@ $('#close-button').on('click', function () {
     });
   }
 })
+//handler for zoom in/out
+$('#zoom-in').on('click', function () {
+  zoom.scaleBy(d3.select("svg").transition().duration(750), 1.2);
+});
+$('#zoom-out').on('click', function () {
+  zoom.scaleBy(d3.select("svg").transition().duration(750), 0.8);
+});
 //setting for select
-$(document).ready(function () {
+$(document).ready(function () {  
+
   jQuery.ajax({
     dataType: "json",
     url: "json/dealer.json",
     async: false,
     success: function (data) { dealers = data }
   });
-  
+
   var data = $.map(dealers, function (obj) {
     obj.id = obj.id; // replace id with your identifier
     obj.text = obj.text || obj.country;
@@ -279,9 +246,9 @@ $(document).ready(function () {
   });
   $('.distor-selector').on('select2:select', function (e) {
     var data = e.params.data;
-    d3.selectAll(".country").classed("country-on", false);
-    d3.selectAll(".country").filter(d => d.properties.iso_n3 == data.id).classed("country-on", true);
-    var j_node = json_data.features.filter(jd => jd.properties.iso_n3 == data.id)[0];    
+    d3.selectAll(".country").style("fill", color_normal);
+    d3.selectAll(".country").filter(d => d.properties.iso_n3 == data.id).style("fill", color_selected);
+    var j_node = json_data.features.filter(jd => jd.properties.iso_n3 == data.id)[0];
     boxZoom(path.bounds(j_node), path.centroid(j_node), 20);
     show_info(j_node.properties.iso_n3);
   });
